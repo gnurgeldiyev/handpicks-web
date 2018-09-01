bfdfb5aee8415a25e19b2ca56eba0787354f92e7
Guvanch <gnb993@yandex.com>
2018-09-01 20:42:34 +0300

    Axios response error handling

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,17 @@
-export default function ({ $axios, app }) {
+export default function ({ $axios, app, route, error, redirect }) {
   const token = app.$cookies.get('token');
   if (token) {
     $axios.setHeader('Authorization', `name=${process.env.clientName}, apiKey=${process.env.clientApiKey}, token=${token}`);
   } else {
     $axios.setHeader('Authorization', `name=${process.env.clientName}, apiKey=${process.env.clientApiKey}`);
   }
+
+  $axios.onResponseError((err) => {
+    if (route.path.indexOf("/p/") > -1) {
+      const code = parseInt(err.response && err.response.status, 10);
+      if (code) {
+        error({ statusCode: 404, message: err.response });
+      }
+    }
+  })
 }
